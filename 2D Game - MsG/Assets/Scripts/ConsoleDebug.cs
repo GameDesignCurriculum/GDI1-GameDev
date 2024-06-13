1119edeea6b4eb9f2d7bf4f715454ad636ded4cf
Ms. Gallant <144454013+thelonges@users.noreply.github.com>
2024-06-13 08:59:16 -0400

    Lesson 3.2.4

---
+++ 2D Game - MsG/Assets/Scripts/ConsoleDebug.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		// Appears in Console
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Hello Game World!");
+
+    }
+    }
+
+/*
+    BRIEFLY DESCRIBE WHAT YOU OBSERVED HERE
+*/
