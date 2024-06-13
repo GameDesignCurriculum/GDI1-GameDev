19ef2ea996018de63648f23f70869487c3b14ac1
Ms. Gallant <144454013+thelonges@users.noreply.github.com>
2024-06-13 09:04:45 -0400

    Lesson 3.2.7

---
+++ 2D Game - MsG/Assets/Scripts/Sprite.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour
+    {
+    public float speed = 2;
+
+    void Start()
+        {
+        Debug.Log("Hello Game World!");
+        }
+
+    void Update()
+        {
+        Vector2 position = transform.position;
+        position += Vector2.right * speed * Time.deltaTime;
+        transform.position = position;
+        }
+    }
+
+/*
+
+    BRIEFLY DESCRIBE YOUR OBSERVATIONS HERE AFTER TESTING IN UNITY
+    
+*/
