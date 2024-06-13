1f7a83c8efb270ac8187ea49bc0f3514f2083071
Ms. Gallant <144454013+thelonges@users.noreply.github.com>
2024-06-13 09:24:57 -0400

    Lesson 3.3.3

---
+++ 2D Game - MsG/Assets/Scripts/TraceVariables.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TraceVariables : MonoBehaviour
+{
+	public string myText = "Enter Text Here";
+	private int coins = 0;
+	private bool isClicked = false;
+	private float speed = 2.3f;
+	
+	// This additional function does something if the player clicks the sprite
+	void OnMouseDown()
+	{
+		isClicked = true;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myText);
+		Debug.Log(speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (isClicked == true)    // if (isClicked) is a shorthand way to write this line
+		{
+			coins = coins + 1;    // coins++; is a shorthand way to write this line
+			Debug.Log(coins);
+			isClicked = false;
+		}
+    }
+}
+
+/*
+
+    BRIEFLY DESCRIBE YOUR OBSERVATIONS HERE AFTER TESTING IN UNITY
+    
+*/
